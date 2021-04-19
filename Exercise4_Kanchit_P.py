d3671c8d73f1f563febe53a9c98ae3c2465c3a0f
namenot45 <kanchit_3310.50@hotmail.com>
2021-04-19 10:18:22 +0700

    Create Exercise4_Kanchit_P.py
    Create Exercise4_Kanchit_P

---
+++ Exercise4_Kanchit_P.py
@@ -0,0 +1,9 @@
+scoreFE = 72.5
+scoreGB = 64
+scoreICS = 80.5
+scoreCP = 84
+print("---Your Score---")
+print("Foundation English :", scoreFE)
+print("General Business :", scoreGB)
+print("Introduction to Computer Systems :", scoreICS)
+print("Computer Programming :", scoreCP)
